ab557e9cbf45b6e37244de6b7d6811078f6446ba
Jackson Nace <jacksonnace964@gmail.com>
2026-02-03 08:51:58 -0600

    assignment 0 4393

--- 4393/PythonScripts/project1/main.py
+++
@@ -1,8 +0,0 @@
-import cv2
-import numpy as np
-
-# black image, white square
-img = np.zeros((512, 512, 3), np.uint8)
-cv2.rectangle(img, (100, 100), (400, 400), (255, 255, 255), -1)
-cv2.imwrite('test_output.png', img)
-print("✅ OpenCV successfully created test_output.png!")
