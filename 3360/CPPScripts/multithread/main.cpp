ec0db56a22631bf4bc3b72bcc0366a378db9a7dd
Jackson Nace <jacksonnace964@gmail.com>
2026-02-03 13:50:06 -0600

    3360

--- 3360/CPPScripts/multithread/main.cpp
+++ 3360/CPPScripts/multithread/main.cpp
@@ -1,21 +1,47 @@
-
 #include <iostream>
-#include <vector>
-#include <unistd.h>    // For fork(), getpid(), sleep()
-#include <sys/wait.h>  // For wait(), waitpid()
-#include <sys/types.h> // For pid_t
-#include <pthread.h>
 #include <string>
-
+#include <vector>
+#include <pthread.h>
 using namespace std;
 
+struct info_from_main{
+  string *message;
+  char ch;
+  unsigned int frequency;
+};
 
+void * frequency_counter(void * void_ptr) {
+  info_from_main * ptr = (info_from_main*) void_ptr;
+  ptr->frequency = 0;
+  for (char c : *(ptr->message)){
+    if (c == ptr->ch)
+      (ptr->frequency)++;
+  }
+  return nullptr;
+}
 
 int main() {
-  string original_message = "message here";
-  string alphabet= "co3 2";
+  string original_message = "COSC 3360 SPRING 2026 CARLOS RINCON CASTRO";
+  string alphabet = "CO3 2";
   int size = alphabet.size();
   vector<unsigned int> freq(size);
-  vector<pthreat_t> tid(size);
+  vector<pthread_t> tid(size);
+  info_from_main result;
+  
+  for (int i=0;i<size;i++) {
+    result.message = &original_message;
+    result.ch = alphabet.at(i);
+    if(pthread_create(&tid.at(i),nullptr,frequency_counter,(void *)&result) != 0)
+    {
+      cerr << "Error creating the thread" << std::endl;
+      exit(0);
+    }
+  }
+  for (int i=0;i<size;i++){
+    pthread_join(tid.at(i),nullptr);
+  }
+  for (int i=0;i<size;i++){
+    std::cout << "Frequency for " << alphabet.at(i) << " = " << freq.at(i) << std::endl;
+  }
   return 0;
 }
